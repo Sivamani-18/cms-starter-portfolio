407e7e3f892af3fcc6c25e62026721287d3bd5cd
Sivamani-18 <siva@mindwaveventures.com>
2024-08-08 18:48:18 +0530

    fix: cache issue

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -28,6 +28,14 @@ const authLink = setContext((_, { headers }) => {
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: 'no-cache',
+    },
+    query: {
+      fetchPolicy: 'no-cache',
+    },
+  },
 });
 
 export default client;

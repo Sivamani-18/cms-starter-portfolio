ef86325faa8f4a51ca5c21338d40c336c6b068f3
Sivamani-18 <siva@mindwaveventures.com>
2024-08-09 22:00:41 +0530

    fix: svg showing issues fixed

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@ const nextConfig = {
   reactStrictMode: true,
   basePath: '',
   assetPrefix: '',
+
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;

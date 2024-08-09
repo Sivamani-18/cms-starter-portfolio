c4d460a212d1958e8b3c7bce00ec6947dc6de6bb
Sivamani-18 <siva@mindwaveventures.com>
2024-08-09 14:12:34 +0530

    update build file

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'export',
   reactStrictMode: true,
+  basePath: '',
+  assetPrefix: '',
 };
 
 export default nextConfig;

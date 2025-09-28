6bf34de3a9851153aa4144cb176ab1f3ba0f4495
Sivasubramaniyam <sivasubramaniyam09@gmail.com>
2025-09-29 01:01:23 +0530

    fix: added header

--- src/app/Components/Header/Header.tsx
+++ src/app/Components/Header/Header.tsx
@@ -1,6 +1,6 @@
+'use client';
 import Link from 'next/link';
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
 import '../../Components/Header/Header.css';
 import ThemeToggle from '../Theme/ThemeToggle';
 import Image from 'next/image';
@@ -16,23 +16,27 @@ export const Header: React.FC<HeaderProps> = ({
   LogoImage = '',
   TextLogo = 'S',
 }) => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => setScrolled(window.scrollY > 8);
+    onScroll();
+    window.addEventListener('scroll', onScroll, { passive: true });
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
+
   return (
-    <header className='site-header' id='site-header'>
+    <header className={`site-header ${scrolled ? 'is-stuck' : ''}`} id='home'>
       <nav className='navbar navbar-expand-xl' id='site-navbar'>
         <div className='container mx-auto flex items-center justify-between py-4'>
           <Link className='navbar-brand' href='/'>
             {LogoImage ? (
-              <Image
-                src={LogoImage}
-                alt='logo'
-                height={50}
-                width={50}
-                unoptimized
-              />
+              <Image src={LogoImage} alt='logo' height={50} width={50} unoptimized />
             ) : (
               <h1 className='text-logo'>{TextLogo}</h1>
             )}
           </Link>
+
           <button
             className='navbar-toggler'
             type='button'
@@ -44,49 +48,19 @@ export const Header: React.FC<HeaderProps> = ({
               <i className='bi bi-list'></i>
             </span>
           </button>
-          <div className='' id='navbarSupportedContent'>
+
+          <div id='navbarSupportedContent'>
             <ul className='navbar-nav'>
-              <li className='nav-item'>
-                <Link
-                  className='nav-link active'
-                  aria-current='page'
-                  href='#site-header'
-                >
-                  Home
-                </Link>
-              </li>
-              <li className='nav-item'>
-                <Link className='nav-link' href='#about-section'>
-                  About
-                </Link>
-              </li>
-              <li className='nav-item'>
-                <Link className='nav-link' href='#portfolio-section'>
-                  Portfolio
-                </Link>
-              </li>
-              <li className='nav-item'>
-                <Link className='nav-link' href='#service-section'>
-                  Service
-                </Link>
-              </li>
-              <li className='nav-item'>
-                <Link className='nav-link' href='#testimonial-section'>
-                  Testimonials
-                </Link>
-              </li>
-              <li className='nav-item'>
-                <Link className='nav-link' href='#blog-section'>
-                  Blog
-                </Link>
-              </li>
-              <li className='nav-item'>
-                <Link className='nav-link' href='#contact-section'>
-                  Contact
-                </Link>
-              </li>
+              <li className='nav-item'><Link className='nav-link active' aria-current='page' href='#home'>Home</Link></li>
+              <li className='nav-item'><Link className='nav-link' href='#about'>About</Link></li>
+              <li className='nav-item'><Link className='nav-link' href='#portfolio'>Portfolio</Link></li>
+              <li className='nav-item'><Link className='nav-link' href='#service'>Service</Link></li>
+              <li className='nav-item'><Link className='nav-link' href='#testimonial'>Testimonials</Link></li>
+              <li className='nav-item'><Link className='nav-link' href='#blog'>Blog</Link></li>
+              <li className='nav-item'><Link className='nav-link' href='#contact'>Contact</Link></li>
             </ul>
           </div>
+
           <ThemeToggle onThemeChange={onThemeChange} />
         </div>
       </nav>
